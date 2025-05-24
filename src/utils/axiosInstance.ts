0e6f3fe2355c98e08e31222b06f2b8ae3279be99
Khayyam Mammadli <khayyamm@Khayyams-MacBook-Air.local>
2025-05-25 00:25:36 +0400

    update

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://interview-backend-p6gz.onrender.com:10000',
+  baseURL: 'https://interview-backend-p6gz.onrender.com',
 });
 
 export default instance;
